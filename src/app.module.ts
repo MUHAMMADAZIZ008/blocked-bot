2c8dfd8af088f5059163c9793b3ea7e654a526df
MUHAMMADAZIZ <muhammadazizgulomov9@gmail.com>
2025-01-28 18:47:40 +0500

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://muhammadaziz:Mm08gulomov@cluster0.or776.mongodb.net/blocked-user',
+      'mongodb+srv://muhammadaziz:Mm08gulomov@cluster0.or776.mongodb.net/blocked-user?retryWrites=true&w=majority&appName=Cluster0',
     ),
     BotModule,
     ScheduleModule.forRoot(),
